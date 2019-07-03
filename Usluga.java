6c6eccfbb4f4f510ab89f264a48b4aa24b2ffaaf
NedicaJovanovic <50808460+NedicaJovanovic@users.noreply.github.com>
2019-07-04 01:22:32 +0200

    Add files via upload

---
+++ Usluga.java
@@ -0,0 +1,38 @@
+package telefonija;
+
+public abstract class Usluga {
+	private Broj inicijator;
+	private Broj primalac;
+	private double cena;
+	
+	public abstract double cenaUsluge();
+	
+	public Usluga(Broj inic, Broj prim) {
+		this.inicijator= inic;
+		this.primalac= prim;
+		
+	}
+	
+	public double getCena() {
+		return cena;
+	}
+	
+	public void setCena(double novaCena) {
+	     cena= novaCena;
+	}
+	
+	public String toString() {
+		String s ="";
+		s+= inicijator;
+		s+= "->" + primalac;	
+		return s;
+	}
+	
+	public Broj getPrim() {
+		return primalac;
+	}
+	
+	public Broj getInic() {
+		return inicijator;
+	}
+}
